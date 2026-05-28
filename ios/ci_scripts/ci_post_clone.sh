f77f49365a3eebac7c8f9aac65042183fa6f6169
Rental <rental@Rentals-MacBook-Air.local>
2026-05-27 18:50:50 -0600

    Fix ci script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
-set -e
-
-echo "Installing Node"
-
-export HOMEBREW_NO_AUTO_UPDATE=1
-brew install node@20
-
-export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
-
-node -v
-npm -v
-
-echo "Installing dependencies"
-
-cd "$CI_PRIMARY_REPOSITORY_PATH"
-
-npm install
-
-echo "Installing pods"
-
+brew install node
+npm install -g yarn
+cd ../..
+yarn install
 cd ios
-pod install --repo-update
+pod install
